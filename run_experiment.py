28408a415422162ed9f1e2a5045b354f65e9919f
MdAsifKhan <mak4086@gmail.com>
2018-08-06 14:49:59 +0200

    final update

--- run_experiment.py
+++ run_experiment.py
@@ -38,7 +38,7 @@ parser.add_argument('--dropout_p', type=float, default=0.2, metavar='',
 					help='Probability of dropping out neuron (default: 0.2)')
 parser.add_argument('--mlp_hidden', type=int, default=100, metavar='',
 					help='size of ER-MLP hidden layer (default: 100)')
-parser.add_argument('--mb_size', type=int, default=50, metavar='',
+parser.add_argument('--mb_size', type=int, default=200, metavar='',
 					help='size of minibatch (default: 50)')
 parser.add_argument('--negative_samples', type=int, default=3, metavar='',
 					help='number of negative samples per positive sample  (default: 3)')
@@ -56,8 +56,7 @@ parser.add_argument('--normalize_embed', default=False, type=bool, metavar='',
 					help='whether to normalize embeddings to unit euclidean ball (default: False)')
 parser.add_argument('--checkpoint_dir', default='models/', metavar='',
 					help='directory to save model checkpoint, saved every epoch (default: models/)')
-parser.add_argument('--use_gpu', default=False, action='store_true',
-					help='whether to run in the GPU')
+parser.add_argument('--use_gpu', default=False, type=bool, metavar='', help='whether to run in the GPU')
 
 args = parser.parse_args()
 
@@ -91,6 +90,8 @@ train_triples = preprocessor.train_triples
 train_idx = preprocessor.triple_to_index(train_triples)
 preprocessor.get_max_len(train_idx)
 train_data = preprocessor.pad_idx_data(train_idx)
+maxlen_s = preprocessor.maxlen_s
+maxlen_o = preprocessor.maxlen_o
 
 pretrained_weights = preprocessor.embedding_matrix()
 embedding_dim = preprocessor.embedding_dim
@@ -145,14 +146,14 @@ if args.model == 'BilinearAvg':
 	model = BilinearModel(embedding_dim=embedding_dim, embedding_rel_dim=embedding_rel_dim, weights=pretrained_weights, n_r=n_r, lw=lw, batch_size=batch_size, input_dropout=dropout_p, gpu=gpu)
 elif args.model == 'BilinearLstm':
 	lstm_ = True
-	model = LSTM_BilinearModel(embedding_dim=embedding_dim, embedding_rel_dim=embedding_rel_dim, weights=pretrained_weights, n_r=n_r, lw=lw, batch_size=batch_size, input_dropout=dropout_p, gpu=gpu)
+	model = LSTM_BilinearModel(embedding_dim=embedding_dim, embedding_rel_dim=embedding_rel_dim, maxlen_s=maxlen_s, maxlen_o=maxlen_o, weights=pretrained_weights, n_r=n_r, lw=lw, batch_size=batch_size, input_dropout=dropout_p, gpu=gpu)
 elif args.model == 'DistMultAvg':
 	model = Avg_DistMult(embedding_dim=embedding_dim, embedding_rel_dim=embedding_rel_dim, weights=pretrained_weights, n_r=n_r, lw=lw, batch_size=batch_size, input_dropout=dropout_p, gpu=gpu)	
 elif args.model == 'DistMultLstm':
-	model = LSTM_DistMult(embedding_dim=embedding_dim, embedding_rel_dim=embedding_rel_dim, weights=pretrained_weights, n_r=n_r, lw=lw, batch_size=batch_size, input_dropout=dropout_p, gpu=gpu)
+	model = LSTM_DistMult(embedding_dim=embedding_dim, embedding_rel_dim=embedding_rel_dim, weights=pretrained_weights, maxlen_s=maxlen_s, maxlen_o=maxlen_o, n_r=n_r, lw=lw, batch_size=batch_size, input_dropout=dropout_p, gpu=gpu)
 	lstm_ = True
 elif args.model == 'ErmlpLstm':
-	model = LSTM_ERMLP(embedding_dim=embedding_dim, embedding_rel_dim=embedding_rel_dim, mlp_hidden=mlp_hidden, weights=pretrained_weights, n_r=n_r, lw=lw, batch_size=batch_size, input_dropout=dropout_p, gpu=gpu)
+	model = LSTM_ERMLP(embedding_dim=embedding_dim, embedding_rel_dim=embedding_rel_dim, mlp_hidden=mlp_hidden, maxlen_s=maxlen_s, maxlen_o=maxlen_o, weights=pretrained_weights, n_r=n_r, lw=lw, batch_size=batch_size, input_dropout=dropout_p, gpu=gpu)
 	lstm_ = True
 elif args.model == 'ErmlpAvg':
 	model = ERMLP_avg(embedding_dim=embedding_dim, embedding_rel_dim=embedding_rel_dim, mlp_hidden=mlp_hidden, weights=pretrained_weights, n_r=n_r, lw=lw, batch_size=batch_size, input_dropout=dropout_p, gpu=gpu)
@@ -162,6 +163,27 @@ else:
 
 optimizer = torch.optim.Adagrad(model.parameters(), lr=lr, lr_decay=lr_decay, weight_decay=weight_decay)
 
+if valid_data[0].shape[0]<batch_size:
+	rf = batch_size-valid_data[0].shape[0]
+	valid_s, valid_o, valid_p = valid_data
+	valid_s = np.vstack([valid_s, valid_s[-rf:]])
+	valid_o = np.vstack([valid_o, valid_o[-rf:]])
+	valid_p = np.concatenate((valid_p, valid_p[-rf:]))
+	valid_label = np.concatenate((valid_label,valid_label[-rf:]))
+
+valid_data = valid_s, valid_o, valid_p, valid_label
+valid_loader = DataLoader(TripleDataset(valid_data,dev=True), batch_size=batch_size,shuffle=True, num_workers=4)
+
+if test_data[0].shape[0]<batch_size:
+	rf = batch_size-test_data[0].shape[0]
+	test_s, test_o, test_p = test_data
+	test_s = np.vstack([test_s, test_s[-rf:]])
+	test_o = np.vstack([test_o, test_o[-rf:]])
+	test_p = np.concatenate((test_p, test_p[-rf:]))
+	test_label = np.concatenate((test_label, test_label[-rf:]))
+
+test_data = test_s, test_o, test_p, test_label
+test_loader = DataLoader(TripleDataset(test_data, dev=True), batch_size=batch_size,shuffle=True, num_workers=4)
 
 for epoch in range(epochs):
 	epoch_loss = []
@@ -169,7 +191,8 @@ for epoch in range(epochs):
 	for i, train_positive_data in enumerate(train_loader,0):
 		model.zero_grad()
 		if lstm_:
-			model.init_hidden()
+			model.hidden1 = model.init_hidden1()
+			model.hidden2 = model.init_hidden2()
 		train_s, train_o, train_p = train_positive_data
 		train_negative_data = sample_negatives(train_positive_data, sampling_factor=sampling_factor)
 		train_neg_s, train_neg_o, train_neg_p = train_negative_data
@@ -182,41 +205,44 @@ for epoch in range(epochs):
 		loss = model.bce_loss(score, train_label, average=True)
 		loss.backward()
 		optimizer.step()
-		optimizer.zero_grad()
 		if normalize_embed:
 			model.normalize_embeddings()
 		epoch_loss.append(loss.cpu().data.numpy())
-
-	if epoch%10 == 0:
 		pred_score = model.predict_proba(score)
 		score = score.cpu().data.numpy() if gpu else score.data.numpy()
 		train_auc_score = auc(score, train_label)
-		print('Epoch {0}\tTrain Loss value: {1}'.format(epoch, stats(epoch_loss)))
-		print('Epoch {0}\tTraining AUC Score: {1}'.format(epoch, train_auc_score))
-
+		print('Epoch {0}\t Batch{1}\tTrain Loss value: {2}'.format(epoch, i, stats(epoch_loss)))
+		print('Epoch {0}\t Batch{1}\tTraining AUC Score: {2}'.format(epoch, i, train_auc_score))
+	
+	if epoch%10 == 0:
 		# Do evaluation on Dev Set		
-		valid_s, valid_o, valid_p = valid_data
-		score_val = model.forward(valid_s, valid_o, valid_p)
-		score_val = score_val.cpu().data.numpy() if gpu else score_val.data.numpy()
-		val_acc, thresh = find_clf_threshold(score_val)
+		for j, valid_batch_data in enumerate(valid_loader,0):
+			valid_s, valid_o, valid_p, valid_label = valid_batch_data
+			score_val = model.forward(valid_s.numpy(), valid_o.numpy(), valid_p.numpy())
+			score_val = score_val.cpu().data.numpy() if gpu else score_val.data.numpy()
+			val_acc, thresh = find_clf_threshold(score_val)
 		
-		print('Threshold {0}'.format(thresh))
-		val_auc_score = auc(score_val, valid_label)
+			print('Threshold {0}'.format(thresh))
+			val_auc_score = auc(score_val, valid_label)
 
-		print('Epoch {0}\tValidation Accuracy: {1}'.format(epoch, val_acc))
-		print('Epoch {0}\tValidation AUC Score: {1}'.format(epoch, val_auc_score))	
+			print('Epoch {0}\t Batch{1}\t Validation Accuracy: {2}'.format(epoch, j, val_acc))
+			print('Epoch {0}\t Batch{1}\t Validation AUC Score: {2}'.format(epoch, j, val_auc_score))	
 
 		# Do evaluation on Dev Set 2
-		test_s, test_o, test_p = test_data
-		score_test = model.forward(test_s, test_o, test_p)
-		score_test = score_test.cpu().data.numpy() if gpu else score_test.data.numpy()
-		test_acc = get_accuracy(score_test, thresh)
-
-		#score_test = score_test.cpu().data.numpy() if gpu else score_test.data.numpy()
-		test_auc_score = auc(score_test, test_label)
-
+		test_acc , test_auc = 0.0, 0.0
+		for k, test_batch_data in enumerate(test_loader,0):
+			test_s, test_o, test_p, test_label = test_batch_data
+			score_test = model.forward(test_s.numpy(), test_o.numpy(), test_p.numpy())
+			score_test = score_test.cpu().data.numpy() if gpu else score_test.data.numpy()
+			acc = get_accuracy(score_test, thresh)
+			#score_test = score_test.cpu().data.numpy() if gpu else score_test.data.numpy()
+			auc_score = auc(score_test, test_label)
+			test_acc += acc
+			test_auc += auc_score
+		test_acc = test_acc/(k+1)
+		test_auc = test_auc/(k+1)
 		print('Epoch {0}\tTest Accuracy: {1}'.format(epoch, test_acc))
-		print('Epoch {0}\tTest AUC Score: {1}'.format(epoch, test_auc_score))	
+		print('Epoch {0}\tTest AUC Score: {1}'.format(epoch, test_auc))	
 
 
 	torch.save(model.state_dict(), checkpoint_path)
